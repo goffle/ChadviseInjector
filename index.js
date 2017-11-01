4e2bbb79a314d7325f698234857718f8a0d7d049
goffle <se.legoff@gmail.com>
2017-11-01 14:33:27 +0800

    fuck

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // this is the code which will be injected into a given page...
 (function (document) {
+	debugger;
 	document.onload = function (e) {
+		debugger;
 		const url = "https://chadvisechatbox-dot-artemis-6031a.appspot.com/?apikey=-Kw-WxphgxRb4uBSgM-_";
 		var wrapper = document.createElement("div");
 		wrapper.innerHTML = '<iframe id="chadvise-id" src=' + url + ' style="visibility: hidden;pointer-events: all;width: 800px;height: 550px;border: 0;position: fixed;z-index: 999;bottom: 0;right: 0;background-color: transparent;border: 1;"/>';

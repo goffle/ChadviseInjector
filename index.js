cc103080edd26d91648440088122bad5501debe5
goffle <se.legoff@gmail.com>
2017-11-01 14:29:31 +0800

    add calll back

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // this is the code which will be injected into a given page...
 (function (document) {
-	const url = "https://chadvisechatbox-dot-artemis-6031a.appspot.com/?apikey=-Kw-WxphgxRb4uBSgM-_";
-	debugger;
-	var wrapper = document.createElement("div");
-	wrapper.innerHTML = '<iframe id="chadvise-id" src=' + url + ' style="visibility: hidden;pointer-events: all;width: 800px;height: 550px;border: 0;position: fixed;z-index: 999;bottom: 0;right: 0;background-color: transparent;border: 1;"/>';
-	document.body.appendChild(wrapper);
-	document.getElementById('chadvise-id').onload = function () {
-		this.style.visibility = 'visible';
-		setTimeout(() => {
-			setInterval(() => {
-				var frame = window.frames['chadvise-id'];
-				frame.contentWindow.postMessage(window.location.href, '*');
-			}, 3000);
-		}, 5000);
-	};
+	window.onload = function (e) {
+		const url = "https://chadvisechatbox-dot-artemis-6031a.appspot.com/?apikey=-Kw-WxphgxRb4uBSgM-_";
+		var wrapper = document.createElement("div");
+		wrapper.innerHTML = '<iframe id="chadvise-id" src=' + url + ' style="visibility: hidden;pointer-events: all;width: 800px;height: 550px;border: 0;position: fixed;z-index: 999;bottom: 0;right: 0;background-color: transparent;border: 1;"/>';
+		document.body.appendChild(wrapper);
+		document.getElementById('chadvise-id').onload = function () {
+			this.style.visibility = 'visible';
+			setTimeout(() => {
+				setInterval(() => {
+					var frame = window.frames['chadvise-id'];
+					frame.contentWindow.postMessage(window.location.href, '*');
+				}, 3000);
+			}, 5000);
+		};
+	}
 })(document);
